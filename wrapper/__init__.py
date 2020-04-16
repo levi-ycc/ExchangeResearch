138d39653c95f106cb11a67069a7d51eb142eda8
Levi <levi.ycc@gmail.com>
2020-04-16 18:44:48 +0800

    refactor code

--- wrapper/__init__.py
+++ wrapper/__init__.py
@@ -1,1 +1,9 @@
-from wrapper.wrapper import *
+from gym import Wrapper
+import numpy as np
+
+class MyWrapper(Wrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        
+    def step(self, action):
+        return self.env.step(action)
--- wrapper/wrapper.py
+++
@@ -1,9 +0,0 @@
-from gym import Wrapper
-import numpy as np
-
-class MyWrapper(Wrapper):
-    def __init__(self, env):
-        super().__init__(env)
-        
-    def step(self, action):
-        return self.env.step(action)
